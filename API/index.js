40ce7cbafdfcf264c015927005338ea74d52c248
Milton <miltonbarrientos2@gmail.com>
2022-12-02 01:27:12 -0600

    el mero start

--- API/index.js
+++ API/index.js
@@ -138,7 +138,9 @@ app.get('/drama-movies', (_req, res) => {
   } catch (error) {}
 });
 
-app.listen(3000, () => {
+// enviroment port or 3000
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   db.connect();
   console.log('Conectado a la base de datos');
   console.log('Example app listening on port 3000!'.bgGreen);
